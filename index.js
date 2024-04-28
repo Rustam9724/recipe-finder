fbffcbe473954f688f3aa086bd5e71c9903cc5a2
rustam_9724 <rustam_9724@mail.ru>
2024-04-28 11:32:13 +0500

    init: page open animation

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ recipeItems.forEach(item => {
         item.style.display = "none"
     }
 })
+
+// Анимация при загрузке страницы
+const title = document.querySelector(".header__title");
+const headerContainer = document.querySelector(".header__container")
+
+title.style.fontSize = window.innerWidth < 605 ? "2.6rem" : "4rem";
+title.style.top = window.innerWidth < 605 ? "16px" : "33px";
+title.style.transform = "translate(-50%, 0)";
